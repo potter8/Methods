d343cf80090763b5ed1a67f065f80754f64aff29
Matthew Potter <potter8@usf.edu>
2021-02-01 10:58:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name:");
+            string input = Console.ReadLine();
+            Console.Write("Hello" + " " + input + "!");
+        }
+    }
+}
